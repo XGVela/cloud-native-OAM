2cb9096b0d93407885504044a086af0ce15dee58
wuzhe <wuzhe02@inspur.com>
2022-11-15 14:40:58 +0800

    clean code

--- oam-network-signaltrace/src/main/java/org.xgvela.oam/service/IOamVnfSignaltraceService.java
+++
@@ -1,9 +0,0 @@
-package org.xgvela.oam.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.xgvela.oam.entity.signaltrace.OamVnfSignaltrace;
-
-
-public interface IOamVnfSignaltraceService extends IService<OamVnfSignaltrace> {
-
-}
